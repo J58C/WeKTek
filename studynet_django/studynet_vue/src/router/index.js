1aa8378cfadbef67b6f5670d6485e134159b7aeb
Mousynano <krnwnfauzi19@gmail.com>
2023-06-01 01:19:44 +0700

    bismillaah

--- studynet_django/studynet_vue/src/router/index.js
+++ studynet_django/studynet_vue/src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
-
+import Editor from '../views/Editor.vue';
 import Courses from '../views/Courses.vue'
 import Course from '../views/Course.vue'
 import Author from '../views/Author.vue'
