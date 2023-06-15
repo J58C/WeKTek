34f6c9798b744f889b7017ef97796cb23532a9ca
Hafidh Musyafa <j5musyaf@gmail.com>
2023-06-15 18:47:13 +0700

    ganti

--- run.bat
+++
@@ -1,6 +0,0 @@
-start cmd.exe /c "cd .\studynet_django\studynet_vue && npm run serve"
-start cmd.exe /c "ping localhost -n 25 -w 60000 > nul && start http://localhost:8080/"
-call .\venv\Scripts\Activate
-cd .\studynet_django
-start /B python manage.py runserver
-cls
--- studynet_django/ide/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.compile_code, name='compile_code'),
-]
